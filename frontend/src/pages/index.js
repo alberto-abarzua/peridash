95f594752c65bbb05d7f67b990ba1400c66a26a9
alberto-abarzua <uintuser@pm.me>
2023-05-13 20:58:23 -0400

    chore: add test first page

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,12 @@
+import RandomNumberGenerator from '../components/RandomNumberGenerator';
+
+const TestPage = () => {
+  return (
+    <div>
+      <h1>Test Page</h1>
+      <RandomNumberGenerator />
+    </div>
+  );
+};
+
+export default TestPage;
