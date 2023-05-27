77102e05e29be89ec5a8476da7947c7886ffd0ed
alberto-abarzua <uintuser@pm.me>
2023-05-27 17:44:05 -0400

    chore: update nginx image

---
+++ nginx/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Substitute environment variables and create nginx.conf
+envsubst '\$SERVER_NAME \$FRONTEND_URL \$BACKEND_URL' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/nginx.conf && nginx -t
+
+# Start nginx in the foreground
+exec nginx -g 'daemon off;'
