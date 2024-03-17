a793df5108aafcd1805008783a84d9659853539b
alberto-abarzua <uintuser@pm.me>
2024-03-17 12:47:12 -0300

    chore: update sagas

--- frontend/src/redux/sagas.js
+++ frontend/src/redux/sagas.js
@@ -1,11 +1,19 @@
 import { all, call } from 'redux-saga/effects';
 import { watchLogout, refreshSessionSaga } from '@/redux/sagas/userSagas';
 import { watchCallUpdateTickers, watchUpdateTickers } from '@/redux/sagas/tickerSagas';
+
+function* startupSaga() {
+    yield takeLatest('REHYDRATION_COMPLETE', function* () {
+        console.log('REHYDRATION_COMPLETE');
+        yield all([
+            call(watchLogout),
+            call(refreshSessionSaga),
+            call(watchCallUpdateTickers),
+            call(watchUpdateTickers),
+        ]);
+    });
+}
+
 export default function* rootSaga() {
-    yield all([
-        call(watchLogout),
-        call(refreshSessionSaga),
-        call(watchCallUpdateTickers),
-        call(watchUpdateTickers),
-    ]);
+    yield all([call(startupSaga)]);
 }
